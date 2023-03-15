6870875ff1226bcca9658aec9883250d74c95b8d
Althail <anthony.duret@epsi.fr>
2023-03-15 14:25:00 +0100

    Push

---
+++ Projet_Mobile_EPSI_2/Projet_Mobile_1/app/src/main/java/fr/epsi/projet_mobile_1/MainActivity.kt
@@ -0,0 +1,11 @@
+package fr.epsi.projet_mobile_1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ Projet_Mobile_EPSI_2/README.md
@@ -0,0 +1,3 @@
+# Projet_Mobile_EPSI
+
+### Le projet mobile 1 se situe sur la branche develop
