0c4aea1101b4ab106bfccaae367928b76cf55e38
sangmin <tkdals4723@gmail.com>
2023-04-16 21:08:36 +0200

    Création de compte Page created

---
+++ Projet_Mobile_EPSI_2/Projet_Mobile_2/app/src/main/java/com/example/projet_mobile_2/InscriptionActivity.kt
@@ -0,0 +1,13 @@
+package com.example.projet_mobile_2
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+
+class InscriptionActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_inscription)
+    }
+}
