169355ca5b46e59af606e07f62c1deaf8f1b0312
sangmin <tkdals4723@gmail.com>
2023-04-21 22:18:21 +0200

    activity_barcode.xml

--- Projet_Mobile_EPSI_2/Projet_Mobile_EPSI_2/Projet_Mobile_2/app/src/main/java/com/example/projet_mobile_2/BarcodeActivity.kt
+++ Projet_Mobile_EPSI_2/Projet_Mobile_EPSI_2/Projet_Mobile_2/app/src/main/java/com/example/projet_mobile_2/BarcodeActivity.kt
@@ -1,57 +1,47 @@
 package com.example.projet_mobile_2
 
+import android.os.Bundle
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
 import com.google.zxing.BarcodeFormat
 import com.google.zxing.EncodeHintType
 import com.google.zxing.MultiFormatWriter
 import com.google.zxing.WriterException
 import com.google.zxing.common.BitMatrix
-import com.google.zxing.oned.Code128Writer
-import com.google.zxing.qrcode.QRCodeWriter
-import com.google.zxing.EncodeHintType
+import java.util.*
 
-fun createBarcode(text: String, format: BarcodeFormat, width: Int, height: Int): BitMatrix? {
-    val hints = mutableMapOf<EncodeHintType, Any>()
-    hints[EncodeHintType.MARGIN] = 0
-    val writer = when (format) {
-        BarcodeFormat.QR_CODE -> QRCodeWriter()
-        BarcodeFormat.CODE_128 -> Code128Writer()
-        else -> throw IllegalArgumentException("Unsupported barcode format")
-    }
-    return try {
-        writer.encode(text, format, width, height, hints)
-    } catch (e: WriterException) {
-        null
-    }
-}
-
-import android.graphics.Bitmap
-import android.graphics.Color
-import android.graphics.Matrix
-import android.graphics.drawable.BitmapDrawable
-import android.os.Bundle
-import android.widget.ImageView
-import androidx.appcompat.app.AppCompatActivity
-
-class BarcodeActivity : AppCompatActivity() {
+class BarcodeActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_barcode)
 
-        val text = "12345"
-        val format = BarcodeFormat.CODE_128
-        val width = 500
-        val height = 200
-        val barcode = createBarcode(text, format, width, height)
-        if (barcode != null) {
-            val bitmap = Bitmap.createBitmap(barcode.width, barcode.height, Bitmap.Config.ARGB_8888)
-            for (x in 0 until barcode.width) {
-                for (y in 0 until barcode.height) {
-                    bitmap.setPixel(x, y, if (barcode.get(x, y)) Color.BLACK else Color.WHITE)
-                }
-            }
-            val imageView = findViewById<ImageView>(R.id.imageView)
-            imageView.setImageBitmap(bitmap)
-        }
-    }
-}
+        val barcode = generateRandomBarcode()
+        val imgBarcode: ImageView = findViewById<ImageView>(R.id.img_barcode)
 
+
+//        imgBarcode.setImageBitmap(barcode)
+    }
+
+
+    fun generateRandomBarcode(): BitMatrix {
+        // Génération d'un code aléatoire de 12 chiffres
+        val random = Random()
+        val barcodeNumber = (1..12).map { random.nextInt(10) }.joinToString("")
+
+        // Configuration de l'encodage du code-barres
+        val hints = EnumMap<EncodeHintType, Any>(EncodeHintType::class.java)
+        hints[EncodeHintType.MARGIN] = 0 // Pas de marge
+
+        try {
+            // Encodage du code-barres avec le format EAN_13
+            val writer = MultiFormatWriter()
+            val bitMatrix = writer.encode(barcodeNumber, BarcodeFormat.EAN_13, 600, 300, hints)
+            return bitMatrix
+        } catch (e: WriterException) {
+            // Gestion des erreurs d'encodage
+            throw RuntimeException("Erreur lors de la génération du code-barres", e)
+        }
+
+    }
+
+}
