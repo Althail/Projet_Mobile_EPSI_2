b6f033cb2c364c91f9b38099e1e83d4a817781a8
sangmin <tkdals4723@gmail.com>
2023-04-24 21:50:19 +0200

    From SplashActivity, we can send user to another page by exsiting or not the database

--- Projet_Mobile_2/app/src/main/java/com/example/projet_mobile_2/MainActivity.kt
+++ Projet_Mobile_2/app/src/main/java/com/example/projet_mobile_2/MainActivity.kt
@@ -5,8 +5,13 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
---
+++ Projet_Mobile_2/app/src/main/java/com/example/projet_mobile_2/User.kt
@@ -0,0 +1,9 @@
+package com.example.projet_mobile_2
+
+data class User(val firstName : String? = null,
+                val lastName : String? = null,
+                val email : String? = null,
+                val address : String? = null,
+                val zipcode : String? = null,
+                val city : String? = null,
+                val cardRef : String? = null)
