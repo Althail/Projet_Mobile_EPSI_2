8b68c58f2185878f0e871750ef9995eec7f84c54
sangmin <tkdals4723@gmail.com>
2023-04-28 00:22:27 +0200

    Card to Magasin Detail connected ... It's awesome !

---
+++ Projet_Mobile_2/app/src/main/java/com/example/projet_mobile_2/MagasinDetailActivity.kt
@@ -0,0 +1,13 @@
+package com.example.projet_mobile_2
+
+import android.os.Bundle
+
+class MagasinDetailActivity: BaseActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_magasindetail)
+        setHeaderTitle("Must Be Nom du Magasin")
+
+
+    }
+}
--- Projet_Mobile_2/app/src/main/java/com/example/projet_mobile_2/MagasinsFragment.kt
+++ Projet_Mobile_2/app/src/main/java/com/example/projet_mobile_2/MagasinsFragment.kt
@@ -2,12 +2,13 @@ package com.example.projet_mobile_2
 
 import android.Manifest
 import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.fragment.app.Fragment
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
@@ -102,9 +103,11 @@ class MagasinsFragment : Fragment() {
 
         }
 
+
         googleMap.setOnInfoWindowClickListener {
             //(activity as BaseActivity).showToast(it.title.toString())
-
+            val intent = Intent(activity, MagasinDetailActivity::class.java)
+            startActivity(intent)
         }
         this.googleMap = googleMap
         locationPermissionRequest.launch(
@@ -117,7 +120,6 @@ class MagasinsFragment : Fragment() {
     }
 
 
-
     private var param1: String? = null
     private var param2: String? = null
 
@@ -127,6 +129,7 @@ class MagasinsFragment : Fragment() {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
         }
+
     }
 
     override fun onCreateView(
