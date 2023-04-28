a8cd2cb4e79472616b91ed5ef4ddf98eab4f69fb
Althail <anthony.duret@epsi.fr>
2023-04-28 19:36:57 +0200

    Offre

---
+++ Projet_Mobile_2/app/src/main/java/com/example/projet_mobile_2/Offres.kt
@@ -0,0 +1,4 @@
+package com.example.projet_mobile_2
+
+class Offres (val name: String,val description: String, val picture_url: String) {
+}
--- Projet_Mobile_2/app/src/main/java/com/example/projet_mobile_2/OffresFragment.kt
+++ Projet_Mobile_2/app/src/main/java/com/example/projet_mobile_2/OffresFragment.kt
@@ -6,6 +6,16 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import okhttp3.CacheControl
+import okhttp3.Call
+import okhttp3.Callback
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.Response
+import org.json.JSONObject
+import java.io.IOException
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -18,19 +28,9 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class OffresFragment : Fragment() {
-    // TODO: Rename and change types of parameters
+
     private var param1: String? = null
     private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -40,24 +40,52 @@ class OffresFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_offres, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment Profile.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            OffresFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val offres= arrayListOf<Offres>()
+        val recyclerviewOffers:RecyclerView=view.findViewById<RecyclerView>(R.id.recyclerviewOffers)
+        recyclerviewOffers.layoutManager= LinearLayoutManager(requireContext())
+        val offresAdapter=OffresAdapter(offres)
+        recyclerviewOffers.adapter=offresAdapter
+
+        val okHttpClient: OkHttpClient = OkHttpClient.Builder().build()
+        val mRequestURL=getString(R.string.offres_data)
+        val request = Request.Builder()
+            .url(mRequestURL)
+            .get()
+            .cacheControl(CacheControl.FORCE_NETWORK)
+            .build()
+        okHttpClient.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                // handle failure
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                val data = response.body?.string()
+
+                if (data != null) {
+                    val jsOffers = JSONObject(data)
+                    val jsArrayOffers = jsOffers.getJSONArray("items")
+                    for (i in 0 until jsArrayOffers.length()) {
+                        val js = jsArrayOffers.getJSONObject(i)
+                        val offre = Offres(
+                            js.optString("name", "not found"),
+                            js.optString("description", "not found"),
+                            js.optString("picture_url", "not found")
+                        )
+                        offres.add(offre)
+                    }
+                    requireActivity().runOnUiThread {
+                        offresAdapter.notifyDataSetChanged()
+                    }
                 }
             }
+        })
+    }
+
+    companion object {
+        // factory method and constants
     }
 
 }
