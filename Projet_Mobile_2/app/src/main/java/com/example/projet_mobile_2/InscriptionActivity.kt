a4bd0752866031d1f055d6ac5460fc8a3431c1e9
sangmin <tkdals4723@gmail.com>
2023-04-30 15:33:55 +0200

    clean code - InscriptionActivity

--- Projet_Mobile_2/app/src/main/java/com/example/projet_mobile_2/InscriptionActivity.kt
+++ Projet_Mobile_2/app/src/main/java/com/example/projet_mobile_2/InscriptionActivity.kt
@@ -11,25 +11,24 @@ import com.example.projet_mobile_2.databinding.ActivityInscriptionBinding
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
 
-class InscriptionActivity : AppCompatActivity() {
+class InscriptionActivity : BaseActivity() {
 
     private lateinit var binding: ActivityInscriptionBinding
     private lateinit var database: DatabaseReference
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityInscriptionBinding.inflate(layoutInflater)
-//        setContentView(R.layout.activity_inscription)
         setContentView(binding.root)
-        val intent = getIntent()
+        val intent = intent
 
         // Inscription EditText
-        val et_first_name: EditText = findViewById<EditText>(R.id.et_first_name)
-        val et_last_name: EditText = findViewById<EditText>(R.id.et_last_name)
-        val et_mail: EditText = findViewById<EditText>(R.id.et_e_mail)
-        val et_address: EditText = findViewById<EditText>(R.id.et_address)
-        val et_zipcode: EditText = findViewById<EditText>(R.id.et_zip_code)
-        val et_city: EditText = findViewById<EditText>(R.id.et_city)
-        val et_cardRef: EditText = findViewById<EditText>(R.id.et_cardRef)
+        val etFirstName: EditText = findViewById<EditText>(R.id.et_first_name)
+        val etLastName: EditText = findViewById<EditText>(R.id.et_last_name)
+        val etMail: EditText = findViewById<EditText>(R.id.et_e_mail)
+        val etAddress: EditText = findViewById<EditText>(R.id.et_address)
+        val etZipCode: EditText = findViewById<EditText>(R.id.et_zip_code)
+        val etCity: EditText = findViewById<EditText>(R.id.et_city)
+        val etCardRef: EditText = findViewById<EditText>(R.id.et_cardRef)
 
         if (intent != null) {
             // Data from QR Code
@@ -42,43 +41,42 @@ class InscriptionActivity : AppCompatActivity() {
             val cardRef = intent.getStringExtra("cardRef")
 
             // SetText
-            et_first_name.setText(firstName)
-            et_last_name.setText(lastName)
-            et_mail.setText(email)
-            et_address.setText(address)
-            et_zipcode.setText(zipcode)
-            et_city.setText(city)
-            et_cardRef.setText(cardRef)
+            etFirstName.setText(firstName)
+            etLastName.setText(lastName)
+            etMail.setText(email)
+            etAddress.setText(address)
+            etZipCode.setText(zipcode)
+            etCity.setText(city)
+            etCardRef.setText(cardRef)
         }
 
-        val btn_register: Button = findViewById<Button>(R.id.btn_register)
-        btn_register.setOnClickListener(View.OnClickListener() {
-            val input_firstName = et_first_name.getText().toString()
-            val input_lastName = et_last_name.getText().toString()
-            val input_email = et_mail.getText().toString()
-            val input_address = et_address.getText().toString()
-            val input_zipcode = et_zipcode.getText().toString()
-            val input_city = et_city.getText().toString()
-            val input_cardRef = et_cardRef.getText().toString()
+        val btnRegister: Button = findViewById<Button>(R.id.btn_register)
+        btnRegister.setOnClickListener(View.OnClickListener() {
+            val inputFirstName = etFirstName.text.toString()
+            val inputLastName = etLastName.text.toString()
+            val inputEmail = etMail.text.toString()
+            val inputAddress = etAddress.text.toString()
+            val inputZipCode = etZipCode.text.toString()
+            val inputCity = etCity.text.toString()
+            val inputCardRef = etCardRef.text.toString()
 
-            database =
-                FirebaseDatabase.getInstance(getString(R.string.db_url)).getReference("Users")
-            val User = User(
-                input_firstName,
-                input_lastName,
-                input_email,
-                input_address,
-                input_zipcode,
-                input_city,
-                input_cardRef
+            database = setUserData("Users")
+            val user = User(
+                inputFirstName,
+                inputLastName,
+                inputEmail,
+                inputAddress,
+                inputZipCode,
+                inputCity,
+                inputCardRef
             )
-            database.child("Users").setValue(User).addOnSuccessListener {
+            database.child("Users").setValue(user).addOnSuccessListener {
                 Toast.makeText(this, "Success", Toast.LENGTH_SHORT).show()
             }.addOnFailureListener { Toast.makeText(this, "Failed", Toast.LENGTH_SHORT).show() }
 
-            var intent_barcode = Intent(application, BarcodeActivity::class.java);
-            intent_barcode.putExtra("name", input_firstName + " " + input_lastName)
-            startActivity(intent_barcode)
+            var intentBarcode = Intent(application, BarcodeActivity::class.java);
+            intentBarcode.putExtra("name", "$inputFirstName $inputLastName")
+            startActivity(intentBarcode)
         })
     }
 }
